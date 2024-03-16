f6c882ab04e78f9203a5315eb98ff17487d4dc56
Pawel <pawel.bornikowski@gmail.com>
2024-03-16 17:50:05 +0100

    feat: add example questions

--- content-script/src/components/Chat.tsx
+++ content-script/src/components/Chat.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Questions from './Questions'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className="space-y-4">
+      <p className="text-xs opacity-70">It's important to know the answers to these questions:</p>
+      <Questions/>
+    </div>
   )
 }
 
