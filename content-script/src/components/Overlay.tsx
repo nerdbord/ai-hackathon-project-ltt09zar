eee7f146613381b8c6338d90cee56a4983a9203c
A1wol <a1wol@protonmail.com>
2024-03-17 00:32:44 +0100

    merge conflict

--- content-script/src/components/Overlay.tsx
+++ content-script/src/components/Overlay.tsx
@@ -5,7 +5,7 @@ import Switcher from '@/switcher/Switcher';
 const Overlay = () => {
   return (
     <div className="bottom-0 right-0 h-[100dvh] w-[380px] bg-[#F2F2F2] dark:bg-[#232428] Overlay py-6 space-y-8 pr-4 pl-8 relative">
-      <span className="absolute top-6 left-2">
+      <span className="absolute top-6 left-2 z-9">
         <Switcher />
       </span>
 
