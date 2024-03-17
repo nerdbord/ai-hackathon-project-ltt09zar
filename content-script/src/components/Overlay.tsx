00607cec6bf0a5fce0639d6c8013787bb632cac8
A1wol <a1wol@protonmail.com>
2024-03-17 01:51:33 +0100

    popup preparation

--- content-script/src/components/Overlay.tsx
+++ content-script/src/components/Overlay.tsx
@@ -1,16 +1,30 @@
 import Tabs from "./Tabs";
 import Content from "./Content";
 import Switcher from '@/switcher/Switcher';
+import { useState } from "react";
+import Popup from "./Popup";
 
 const Overlay = () => {
+  const [isPopupVisible, setVisibility] = useState(false)
+
   return (
     <div className="bottom-0 right-0 h-[100dvh] w-[380px] bg-[#F2F2F2] dark:bg-[#232428] Overlay py-6 space-y-8 pr-4 pl-8 relative">
       <span className="absolute top-6 left-2 z-9">
         <Switcher />
       </span>
-
       <Tabs />
       <Content />
+
+      <span className="absolute bottom-8 right-2">
+        <a className="text-black" target="_blank" onClick={() => {
+          setVisibility(!isPopupVisible)
+        }}>
+          Dont click there</a>
+
+        {isPopupVisible && (
+          <Popup />
+        )}
+      </span>
     </div>
   );
 };
---
+++ content-script/src/components/Popup.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Popup = () => {
+  return (
+    <div className="bottom-0 right-0 h-[100dvh] w-[380px] space-y-8 pr-4 pl-8 relative bg-orange z-99">
+      <div className="py-6 bg-orange">
+
+      </div>
+    </div>
+  )
+};
+
+export default Popup;
