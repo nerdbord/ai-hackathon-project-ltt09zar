fab51d0200795ddbe435248d6127215259c2980e
A1wol <a1wol@protonmail.com>
2024-03-17 02:02:05 +0100

    dark mode added

--- content-script/src/components/Overlay.tsx
+++ content-script/src/components/Overlay.tsx
@@ -8,14 +8,15 @@ const Overlay = () => {
   const [isPopupVisible, setVisibility] = useState(false)
 
   return (
-    <div className="bottom-0 right-0 h-[100dvh] w-[380px] bg-[#F2F2F2] dark:bg-[#232428] Overlay py-6 space-y-8 pr-4 pl-8 relative">
+    <div className="bottom-0 overflow-auto right-0 h-[100dvh] w-[380px] bg-[#F2F2F2] dark:bg-[#232428] Overlay py-6 space-y-8 pr-4 pl-8 relative">
       <span className="absolute top-6 left-2 z-9">
         <Switcher />
       </span>
+
       <Tabs />
       <Content />
 
-      <span className="absolute bottom-8 right-2">
+      <span className="absolute bottom-0 right-2">
         <a className="text-black" target="_blank" onClick={() => {
           setVisibility(!isPopupVisible)
         }}>
